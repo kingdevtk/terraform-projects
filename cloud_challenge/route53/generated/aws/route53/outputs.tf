a3dda3d7c0b2ddde2248ebce8c0a3b1b6d255d85
kingdevtk <trina.t.king@gmail.com>
2023-03-27 17:14:34 -0400

    getting close and added more terraform config for apigw, cloudfront, dynamodb

---
+++ cloud_challenge/route53/generated/aws/route53/outputs.tf
@@ -0,0 +1,51 @@
+output "aws_route53_record_tfer--Z03412111SKDXP1DHG5AP__8f4a1e119107f5ad7115c5e171ab3d7d-002E-trinatking-002E-com-002E-_CNAME__id" {
+  value = "${aws_route53_record.tfer--Z03412111SKDXP1DHG5AP__8f4a1e119107f5ad7115c5e171ab3d7d-002E-trinatking-002E-com-002E-_CNAME_.id}"
+}
+
+output "aws_route53_record_tfer--Z03412111SKDXP1DHG5AP__c095b7e078a549af6bb1e7a3e42cfb1e-002E-www-002E-trinatking-002E-com-002E-_CNAME__id" {
+  value = "${aws_route53_record.tfer--Z03412111SKDXP1DHG5AP__c095b7e078a549af6bb1e7a3e42cfb1e-002E-www-002E-trinatking-002E-com-002E-_CNAME_.id}"
+}
+
+output "aws_route53_record_tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com-002E-_A__id" {
+  value = "${aws_route53_record.tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com-002E-_A_.id}"
+}
+
+output "aws_route53_record_tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com-002E-_NS__id" {
+  value = "${aws_route53_record.tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com-002E-_NS_.id}"
+}
+
+output "aws_route53_record_tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com-002E-_SOA__id" {
+  value = "${aws_route53_record.tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com-002E-_SOA_.id}"
+}
+
+output "aws_route53_record_tfer--Z03412111SKDXP1DHG5AP_www-002E-trinatking-002E-com-002E-_A__id" {
+  value = "${aws_route53_record.tfer--Z03412111SKDXP1DHG5AP_www-002E-trinatking-002E-com-002E-_A_.id}"
+}
+
+output "aws_route53_record_tfer--Z064000611SQH1FUMQRAZ_ablackladytechshow-002E-com-002E-_NS__id" {
+  value = "${aws_route53_record.tfer--Z064000611SQH1FUMQRAZ_ablackladytechshow-002E-com-002E-_NS_.id}"
+}
+
+output "aws_route53_record_tfer--Z064000611SQH1FUMQRAZ_ablackladytechshow-002E-com-002E-_SOA__id" {
+  value = "${aws_route53_record.tfer--Z064000611SQH1FUMQRAZ_ablackladytechshow-002E-com-002E-_SOA_.id}"
+}
+
+output "aws_route53_record_tfer--Z067254110X861QJ0ZUWL_ablackladytechblog-002E-com-002E-_NS__id" {
+  value = "${aws_route53_record.tfer--Z067254110X861QJ0ZUWL_ablackladytechblog-002E-com-002E-_NS_.id}"
+}
+
+output "aws_route53_record_tfer--Z067254110X861QJ0ZUWL_ablackladytechblog-002E-com-002E-_SOA__id" {
+  value = "${aws_route53_record.tfer--Z067254110X861QJ0ZUWL_ablackladytechblog-002E-com-002E-_SOA_.id}"
+}
+
+output "aws_route53_zone_tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com_id" {
+  value = "${aws_route53_zone.tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com.id}"
+}
+
+output "aws_route53_zone_tfer--Z064000611SQH1FUMQRAZ_ablackladytechshow-002E-com_id" {
+  value = "${aws_route53_zone.tfer--Z064000611SQH1FUMQRAZ_ablackladytechshow-002E-com.id}"
+}
+
+output "aws_route53_zone_tfer--Z067254110X861QJ0ZUWL_ablackladytechblog-002E-com_id" {
+  value = "${aws_route53_zone.tfer--Z067254110X861QJ0ZUWL_ablackladytechblog-002E-com.id}"
+}
---
+++ cloud_challenge/s3/generated/aws/s3/s3_bucket.tf
@@ -0,0 +1,276 @@
+resource "aws_s3_bucket" "tfer--ablackladytechblog-002E-com" {
+  bucket        = "ablackladytechblog.com"
+  force_destroy = "false"
+
+  grant {
+    id          = "2d1a4075a347cdb41a154eb6a2d4debe3b805b4711bb475f6d45f676ccae70f8"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  object_lock_enabled = "false"
+
+  policy = <<POLICY
+{
+  "Id": "PolicyForPublicWebsiteContent",
+  "Statement": [
+    {
+      "Action": "s3:GetObject",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Resource": "arn:aws:s3:::ablackladytechblog.com/*",
+      "Sid": "PublicReadGetObject"
+    }
+  ],
+  "Version": "2008-10-17"
+}
+POLICY
+
+  request_payer = "BucketOwner"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+
+      bucket_key_enabled = "false"
+    }
+  }
+
+  versioning {
+    enabled    = "true"
+    mfa_delete = "false"
+  }
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "tfer--trinatking-002E-com" {
+  bucket        = "trinatking.com"
+  force_destroy = "false"
+
+  grant {
+    id          = "2d1a4075a347cdb41a154eb6a2d4debe3b805b4711bb475f6d45f676ccae70f8"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  object_lock_enabled = "false"
+
+  policy = <<POLICY
+{
+  "Id": "PolicyForCloudFrontPrivateContent",
+  "Statement": [
+    {
+      "Action": "s3:GetObject",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E1DPEDO85H0OYN"
+      },
+      "Resource": "arn:aws:s3:::trinatking.com/*",
+      "Sid": "1"
+    }
+  ],
+  "Version": "2008-10-17"
+}
+POLICY
+
+  request_payer = "BucketOwner"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+
+      bucket_key_enabled = "false"
+    }
+  }
+
+  tags = {
+    "trinatking.com" = "resume site"
+  }
+
+  tags_all = {
+    "trinatking.com" = "resume site"
+  }
+
+  versioning {
+    enabled    = "true"
+    mfa_delete = "false"
+  }
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "tfer--trinatking-tf-state" {
+  bucket        = "trinatking-tf-state"
+  force_destroy = "false"
+
+  grant {
+    id          = "2d1a4075a347cdb41a154eb6a2d4debe3b805b4711bb475f6d45f676ccae70f8"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  object_lock_enabled = "false"
+
+  policy = <<POLICY
+{
+  "Statement": [
+    {
+      "Action": "s3:ListBucket",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::438207642063:root"
+      },
+      "Resource": "arn:aws:s3:::trinatking-tf-state"
+    },
+    {
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject"
+      ],
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::438207642063:root"
+      },
+      "Resource": [
+        "arn:aws:s3:::trinatking-tf-state/api-gateway/",
+        "arn:aws:s3:::trinatking-tf-state/cloudfront/",
+        "arn:aws:s3:::trinatking-tf-state/dynamodb/",
+        "arn:aws:s3:::trinatking-tf-state/lambda/",
+        "arn:aws:s3:::trinatking-tf-state/route53/",
+        "arn:aws:s3:::trinatking-tf-state/s3/"
+      ]
+    }
+  ],
+  "Version": "2012-10-17"
+}
+POLICY
+
+  request_payer = "BucketOwner"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+
+      bucket_key_enabled = "true"
+    }
+  }
+
+  versioning {
+    enabled    = "true"
+    mfa_delete = "false"
+  }
+}
+
+resource "aws_s3_bucket" "tfer--www-002E-ablackladytechblog-002E-com" {
+  bucket        = "www.ablackladytechblog.com"
+  force_destroy = "false"
+
+  grant {
+    id          = "2d1a4075a347cdb41a154eb6a2d4debe3b805b4711bb475f6d45f676ccae70f8"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  object_lock_enabled = "false"
+
+  policy = <<POLICY
+{
+  "Id": "PolicyForPublicWebsiteContent",
+  "Statement": [
+    {
+      "Action": "s3:GetObject",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Resource": "arn:aws:s3:::www.ablackladytechblog.com/*",
+      "Sid": "PublicReadGetObject"
+    }
+  ],
+  "Version": "2008-10-17"
+}
+POLICY
+
+  request_payer = "BucketOwner"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+
+      bucket_key_enabled = "false"
+    }
+  }
+
+  versioning {
+    enabled    = "true"
+    mfa_delete = "false"
+  }
+}
+
+resource "aws_s3_bucket" "tfer--www-002E-trinatking-002E-com" {
+  bucket        = "www.trinatking.com"
+  force_destroy = "false"
+
+  grant {
+    id          = "2d1a4075a347cdb41a154eb6a2d4debe3b805b4711bb475f6d45f676ccae70f8"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  object_lock_enabled = "false"
+
+  policy = <<POLICY
+{
+  "Id": "PolicyForPublicWebsiteContent",
+  "Statement": [
+    {
+      "Action": "s3:GetObject",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Resource": "arn:aws:s3:::www.trinatking.com/*",
+      "Sid": "PublicReadGetObject"
+    }
+  ],
+  "Version": "2008-10-17"
+}
+POLICY
+
+  request_payer = "BucketOwner"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+
+      bucket_key_enabled = "false"
+    }
+  }
+
+  versioning {
+    enabled    = "true"
+    mfa_delete = "false"
+  }
+
+  website {
+    redirect_all_requests_to = "http://trinatking.com"
+  }
+}
