51a2c320dd675655f94e11f706a3000b177c8af7
kingdevtk <trina.t.king@gmail.com>
2023-03-28 14:30:29 -0400

    update route53 tf config

---
+++ cloud_challenge/route53/route53_zone.tf
@@ -0,0 +1,5 @@
+resource "aws_route53_zone" "tfer--Z03412111SKDXP1DHG5AP_trinatking-002E-com" {
+  comment       = "HostedZone created by Route53 Registrar"
+  force_destroy = "false"
+  name          = "trinatking.com"
+}
