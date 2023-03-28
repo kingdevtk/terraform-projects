4db1e580fab6bd0493cc5be067b21e08aec382e7
kingdevtk <trina.t.king@gmail.com>
2023-03-28 09:05:34 -0400

    add lambda tf config

--- cloud_challenge/lambda/lambda_function.tf
+++ cloud_challenge/lambda/lambda_function.tf
@@ -5,13 +5,16 @@ resource "aws_lambda_function" "tfer--Visitors" {
     size = "512"
   }
 
+  filename                       = null
   function_name                  = "Visitors"
   handler                        = "lambda_function.handler"
+  image_uri                      = ""
   memory_size                    = "128"
   package_type                   = "Zip"
   reserved_concurrent_executions = "-1"
   role                           = "arn:aws:iam::438207642063:role/lambda"
   runtime                        = "python3.9"
+  s3_bucket                      = null
   skip_destroy                   = "false"
   source_code_hash               = "rLzZNRGSF1hIvSoUPpumS7xEGr75b25BMcreTBOn1i0="
   timeout                        = "3"
