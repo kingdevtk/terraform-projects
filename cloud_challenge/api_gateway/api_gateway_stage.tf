57cf70807fb17253ddda60972d7ec655950c2bf4
kingdevtk <trina.t.king@gmail.com>
2023-04-05 15:27:47 -0400

    update apigw stage cache cluster setting

--- cloud_challenge/api_gateway/api_gateway_stage.tf
+++ cloud_challenge/api_gateway/api_gateway_stage.tf
@@ -1,5 +1,6 @@
 resource "aws_api_gateway_stage" "tfer--g21z1pb3pa-002F-prod" {
   cache_cluster_enabled = "false"
+  cache_cluster_size    = "0.5"
   deployment_id         = "iep57f"
   rest_api_id           = "g21z1pb3pa"
   stage_name            = "prod"
--- cloud_challenge/api_gateway/outputs.tf
+++ cloud_challenge/api_gateway/outputs.tf
@@ -1,55 +1,55 @@
 output "aws_api_gateway_integration_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET_id" {
-  value = "${aws_api_gateway_integration.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET.id}"
+  value = aws_api_gateway_integration.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET.id
 }
 
 output "aws_api_gateway_integration_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS_id" {
-  value = "${aws_api_gateway_integration.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS.id}"
+  value = aws_api_gateway_integration.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS.id
 }
 
 output "aws_api_gateway_integration_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST_id" {
-  value = "${aws_api_gateway_integration.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST.id}"
+  value = aws_api_gateway_integration.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST.id
 }
 
 output "aws_api_gateway_method_response_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET-002F-200_id" {
-  value = "${aws_api_gateway_method_response.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET-002F-200.id}"
+  value = aws_api_gateway_method_response.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET-002F-200.id
 }
 
 output "aws_api_gateway_method_response_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS-002F-200_id" {
-  value = "${aws_api_gateway_method_response.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS-002F-200.id}"
+  value = aws_api_gateway_method_response.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS-002F-200.id
 }
 
 output "aws_api_gateway_method_response_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST-002F-200_id" {
-  value = "${aws_api_gateway_method_response.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST-002F-200.id}"
+  value = aws_api_gateway_method_response.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST-002F-200.id
 }
 
 output "aws_api_gateway_method_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET_id" {
-  value = "${aws_api_gateway_method.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET.id}"
+  value = aws_api_gateway_method.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-GET.id
 }
 
 output "aws_api_gateway_method_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS_id" {
-  value = "${aws_api_gateway_method.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS.id}"
+  value = aws_api_gateway_method.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-OPTIONS.id
 }
 
 output "aws_api_gateway_method_tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST_id" {
-  value = "${aws_api_gateway_method.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST.id}"
+  value = aws_api_gateway_method.tfer--g21z1pb3pa-002F-n8bg4d4t5k-002F-POST.id
 }
 
 output "aws_api_gateway_model_tfer--7fqmff_id" {
-  value = "${aws_api_gateway_model.tfer--7fqmff.id}"
+  value = aws_api_gateway_model.tfer--7fqmff.id
 }
 
 output "aws_api_gateway_model_tfer--inq6k0_id" {
-  value = "${aws_api_gateway_model.tfer--inq6k0.id}"
+  value = aws_api_gateway_model.tfer--inq6k0.id
 }
 
 output "aws_api_gateway_resource_tfer--n8bg4d4t5k_id" {
-  value = "${aws_api_gateway_resource.tfer--n8bg4d4t5k.id}"
+  value = aws_api_gateway_resource.tfer--n8bg4d4t5k.id
 }
 
 output "aws_api_gateway_rest_api_tfer--g21z1pb3pa_visitors_id" {
-  value = "${aws_api_gateway_rest_api.tfer--g21z1pb3pa_visitors.id}"
+  value = aws_api_gateway_rest_api.tfer--g21z1pb3pa_visitors.id
 }
 
 output "aws_api_gateway_stage_tfer--g21z1pb3pa-002F-prod_id" {
-  value = "${aws_api_gateway_stage.tfer--g21z1pb3pa-002F-prod.id}"
+  value = aws_api_gateway_stage.tfer--g21z1pb3pa-002F-prod.id
 }
